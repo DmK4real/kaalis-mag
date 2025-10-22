da4ba205511f393111b3113333a8dfb2fb70f53d
DmK4real <127308800+DmK4real@users.noreply.github.com>
2025-10-22 17:48:06 +0000

    chore: remove Événements from nav

---
+++ lib/nav.ts
@@ -0,0 +1,8 @@
+export const NAV = [
+  { href: '/lieux', label: 'Lieux' },
+  { href: '/personnalites', label: 'Personnalités' },
+  { href: '/style', label: 'Style' },
+  { href: '/culture', label: 'Culture & Opinion' },
+  { href: '/communaute', label: 'Communauté' },
+ 
+]
